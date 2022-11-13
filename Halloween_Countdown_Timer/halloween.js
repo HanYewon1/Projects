82297e257e11299b45ef46cfa1ea8c78a203324a
Yewon Han <62415263+HanYewon1@users.noreply.github.com>
2022-11-13 16:11:02 +0900

    Add files via upload

---
+++ Halloween_Countdown_Timer/halloween.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const halloween = '31 Oct 2022';
+
+function countdown(){
+    const halloweenDate = new Date(halloween);
+    const currentDate = new Date();
+
+    const totalseconds = (halloweenDate - currentDate) / 1000;
+
+    const days = Math.floor(totalseconds / 3600 / 24);
+    const hours = Math.floor(totalseconds / 3600) % 24;
+    const minutes = Math.floor(totalseconds / 60) % 60;
+    const seconds = Math.floor(totalseconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
